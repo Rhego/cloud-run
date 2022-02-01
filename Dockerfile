ca98c9e833a5098ba7d0cbb75340fe34e459edf6
ArnyRincones <arnyrincones@gmail.com>
2022-02-01 14:10:22 -0400

    test mysql

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python
 RUN apt-get update
 RUN apt-get install -y python python3-pip wget
 RUN pip install Flask
+RUN pip install flask_mysqldb
 RUN mkdir /app
 COPY . /app/
 WORKDIR app
--- randomapp.py
+++ randomapp.py
@@ -1,12 +1,31 @@
-# randomapp.py
-import random
-import string
-from flask import Flask
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
 app = Flask(__name__)
-@app.route('/')
-def rannum():
-    x = random.randint(5000770880, 9008800000)
-    y = str(x)
-    return y
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+ 
+app.config['MYSQL_HOST'] = '192.168.102.4'
+app.config['MYSQL_USER'] = 'cloudrun'
+app.config['MYSQL_PASSWORD'] = 'cloudrun'
+app.config['MYSQL_DB'] = 'cloud_run_test'
+ 
+mysql = MySQL(app)
+ 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+ 
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return "Login via the login Form"
+     
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        cursor = mysql.connection.cursor()
+        cursor.execute(''' INSERT INTO users VALUES(%s,%s)''',(name,age))
+        mysql.connection.commit()
+        cursor.close()
+        return f"Done!!"
+ 
+app.run(host='localhost', port=5000)
